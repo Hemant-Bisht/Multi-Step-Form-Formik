afb4b0429bcf5430dd2531a6e02217f8bbf13c79
Hemant-Bisht <singhhemant233@gmail.com>
2021-02-07 13:25:59 +0530

    my second commit

--- src/components/Success.js
+++ src/components/Success.js
@@ -14,7 +14,7 @@ export const Success = () => {
     <div className={classes.textCenter}>
       <Header title='Success' />
       <h1>Thank You For Your Submission</h1>
-      <p>You will get an email with further instructions</p>
+      <p>You will get an email with further instructionsss</p>
     </div>
   );
 };
